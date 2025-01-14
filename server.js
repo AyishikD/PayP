90ce9d9dc860b3b96704acc5951a5d406dc6f98c
AyishikD <99983449+AyishikD@users.noreply.github.com>
2025-01-14 23:52:12 +0530

    Minor change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const paymentRoutes = require('./routes/paymentRoutes');
 const { logger } = require('./utils/logger');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 3000;
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
